052af204f7edf4c2630f41a2a52a4eeb5fae3c9b
moses946 <mosesmwaiw@gmail.com>
2023-03-21 19:36:04 +0300

    Fix error in file 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,18 @@
 #include "main.h"
 /**
   * print_last_digit - prints last digit of a number
-  * @c - number under evaluation
+  * @c: number under evaluation
   * Return: last digit of a number
   */
 int print_last_digit(int c)
 {
 	int last;
+	char p;
 
 	last = c % 10;
 	if (last < 0)
 		last  = last * -1;
+	p = last + '0';
+	_putchar(p);
 	return (last);
 }
