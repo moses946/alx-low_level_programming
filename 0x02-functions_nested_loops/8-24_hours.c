2574eb6a17584459803c90214a90f6529d2d1d9c
moses946 <mosesmwaiw@gmail.com>
2023-03-22 13:10:56 +0300

    Fix error in 8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,19 +10,36 @@ void jack_bauer(void)
 	int k;
 	int l;
 
-	for (i = 0; i < 3; i++)
+	for (i = 0; i <= 2; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			for (k = 0; k <= 9; k++)
+			if (i == 2 && j == 4)
+			{
+				_putchar(i +'0');
+				_putchar(j + '0');
+				_putchar(':');
+				_putchar(k + '0');
+				_putchar(l + '0');
+				_putchar('\n');
+				break;
+			for (k = 0; k <= 5; k++)
 			{
 				for (l = 0; l <= 9; l++)
 				{
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(l + '0');
+					if (i == 2 && j == 4 && l == 1)
+					{
+						break;
+					}
+					else
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(':');
+						_putchar(k + '0');
+						_putchar(l + '0');
+						_putchar('\n');
+					}
 				}
 			}
 		}
