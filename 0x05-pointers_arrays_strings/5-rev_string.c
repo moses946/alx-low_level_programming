f01bdd2d22853ba25e7b8693a34b0ba99b4ede39
moses946 <mosesmwaiw@gmail.com>
2023-03-28 17:25:30 +0300

    add file 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,14 +3,16 @@
   * print_rev - prints a string in reverse
   * @s: string to be reversed
   */
-void print_rev(char *s)
+void rev_string(char *s)
 {
 	int i;
+	char *t;
 	
 	for (i = 0; s[i] != '\0'; i++)
 	{}
 	for (i = i; i >= 0; i--)
 	{
-		_putchar(s[i]);
+		t[i] = s[i];
 	}
+	*s  = *t;
 }
