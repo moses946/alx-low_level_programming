4a0282830eda9347385a108be3531e153ebc407c
moses946 <mosesmwaiw@gmail.com>
2023-04-13 01:36:38 +0300

    Fix error

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+/**
+  * _strlen - Returns length of a string
+  * @s: string evaluated
+  * Return: Length of string
+  */
+int _strlen(char *s)
+{
+	int i;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
+}
 /**
   * _strdup - copies a string given to it as a parameter
   * @str: string to be copied
@@ -12,12 +24,12 @@ char *_strdup(char *str)
 	long unsigned int i;
 	char *nstr;
 
-	nstr = malloc(sizeof(str));
+	nstr = malloc(_strlen(str));
 	if (str == NULL || nstr == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < sizeof(str); i++)
+	for (i = 0; i < _strlen(str); i++)
 	{
 		nstr[i] = str[i];
 	}
