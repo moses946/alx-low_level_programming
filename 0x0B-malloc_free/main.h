73c6371c2e5caa523f713c8a9e997fef1250a1f7
moses946 <mosesmwaiw@gmail.com>
2023-04-13 01:27:05 +0300

    Add file 4-free_grid.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,6 +8,6 @@ char *str_concat(char *s1, char *s2);
 int _strlen(char *s);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
-
+void free_grid(int **grid, int height);
 
 #endif /*MAIN_H*/
