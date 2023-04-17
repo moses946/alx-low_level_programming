dc55b327830228c6bba2c63bfde95eafa51caf2f
moses946 <mosesmwaiw@gmail.com>
2023-04-17 19:18:55 +0300

    Fix errors

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,2 +1,6 @@
+#ifndef _HEADER_
+#define _HEADER_
+
 #include <stdlib.h>
 #define ABS(x) (x * -1)? x<0 : x
+#endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,2 +1,6 @@
+#ifndef _HEADER_
+#define _HEADER_
+
 #include <stdlib.h>
 #define SUM(x, y) ((x) + (y))
+#endif
