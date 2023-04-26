859ca1e8e312685198540dc53195df7edadc1a92
moses946 <mosesmwaiw@gmail.com>
2023-04-26 18:31:13 +0300

    Fix errors

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,17 +6,18 @@
   * @n: Integer to be printed
   *
   */
-void print_number(int n) {
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-
-    if (n / 10) {
-        print_number(n / 10);
-    }
-
-    _putchar(n % 10 + '0');
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
 }
 /**
   * print_numbers - prints numbers followed by a new line
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,20 @@
 #define _HEADER_
 
 #include <stdio.h>
+/**
+  * _puts - prints a string
+  * @str: string to be printed
+  */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+}
+void _puts(char *str);
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
