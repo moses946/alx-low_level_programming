a081162cd9d4a0c6c39df07677cc68f4924715a8
moses946 <mosesmwaiw@gmail.com>
2023-05-02 17:36:13 +0300

    Fix error in 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
  * main - Entry point
  * @argc: Number of arguments
@@ -17,7 +16,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi[argv[1]];
+	cents = atoi(argv[1]);
 	if (cents < 0)
 	{
 		printf("0\n");
