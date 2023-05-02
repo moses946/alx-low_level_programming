009dddef042cf797d1db3d579122c7ad30995a38
moses946 <mosesmwaiw@gmail.com>
2023-05-02 17:09:46 +0300

    Fix asm code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,45 +1,18 @@
-	.file	"ass.c"
-	.text
-	.section	.rodata
-.LC0:
-	.string	"Hello, Holberton"
-	.text
-	.globl	main
-	.type	main, @function
+section .data
+    msg db "Hello, Holberton", 0Ah  ; message to print with newline
+    fmt db "%s"                    ; format string for printf
+
+section .text
+    global main
+    extern printf
+
 main:
-.LFB0:
-	.cfi_startproc
-	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	leaq	.LC0(%rip), %rax
-	movq	%rax, %rdi
-	call	puts@PLT
-	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 11.3.0-1ubuntu1~22.04) 11.3.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	1f - 0f
-	.long	4f - 1f
-	.long	5
-0:
-	.string	"GNU"
-1:
-	.align 8
-	.long	0xc0000002
-	.long	3f - 2f
-2:
-	.long	0x3
-3:
-	.align 8
-4:
+    ; Call printf function to print the message
+    push msg      ; push the message to the stack
+    push fmt      ; push the format string to the stack
+    call printf   ; call printf function to print the message
+    add rsp, 16   ; clean up the stack
+
+    ; Return 0 to indicate successful program execution
+    mov eax, 0
+    ret
