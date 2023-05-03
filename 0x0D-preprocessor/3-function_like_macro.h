97a447793d725895eb1389949da63c63dc7af959
Moses Mwai <67924208+moses946@users.noreply.github.com>
2023-05-03 23:57:00 +0300

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef _HEADER_
-#define _HEADER_
-
-#include <stdlib.h>
-#define ABS(x) ((x < 0) ? (x * -1) : (x))
-#endif
--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,6 +0,0 @@
-#ifndef _HEADER_
-#define _HEADER_
-
-#include <stdlib.h>
-#define SUM(x, y) ((x) + (y))
-#endif
