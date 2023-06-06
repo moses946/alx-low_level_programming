e12a141161f8d382aa39e9757db9a5a012efac64
moses946 <mosesmwaiw@gmail.com>
2023-06-07 00:37:58 +0300

    Add main.h to file 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include "main.h"
 /**
   * print_chessboard - Prints the chessboard
-  * a: The board to be printed
+  *@a:The board to be printed
   */
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
-	
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
