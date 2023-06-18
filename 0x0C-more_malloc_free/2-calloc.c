2e249faec6b2b376962e57ee390e5c1d8c1e0abe
moses946 <mosesmwaiw@gmail.com>
2023-06-18 16:12:34 +0300

    Rewrite entire file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,25 +2,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-  * _calloc - Allocates memory for an array
-  * @nmemb: Number of array elements
+  * _calloc - Allocates memory for an array of n members of size bytes each
+  * @nmemb: Number of elements
   * @size: Size of each element
-  * Return: Pointer to the allocated memory
+  *
+  * Return: Pointer to allocated memory
   */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *array;
-	unsigned int i = 0;
+	void *array;
+	unsigned int i;
+	char *char_array;
+	int *int_array;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
 	array = malloc(nmemb * size);
 	if (!array)
 		return (NULL);
-	while (i <= nmemb)
+	if (size == 1)
 	{
-		array[i] = '\0';
-		i++;
+		char_array = array;
+		for (i = 0; i < nmemb; i++)
+			char_array[i] = 0;
+	}
+	else if (size == 4)
+	{
+		int_array = array;
+		for (i = 0; i < nmemb; i++)
+			int_array[i] = 0;
 	}
 	return (array);
 }
+
