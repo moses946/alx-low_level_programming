7b59b2f1d1a66f9e22b42263df031cfe8878e140
moses946 <mosesmwaiw@gmail.com>
2023-06-18 15:14:25 +0300

    Rewrite whole file

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,27 +1,28 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 /**
   * array_range - Creates an array of integers
   * @min: Minimum value
   * @max: Maximum value
   *
-  * Return: Pointer to array or NULL if it fails
+  * Return: Pointer to newly formed array
   */
 int *array_range(int min, int max)
 {
-	int *array, size, i = 0;
+	int *array, size, counter = 0;
 
-	size = (max - min) + 1;
 	if (min > max)
 		return (NULL);
+	size = (max - min) + 1;
 	array = malloc(sizeof(int) * size);
 	if (!array)
 		return (NULL);
-	while (i <= size)
+	while (counter <= size)
 	{
-		array[i] = min + i;
-		i++;
+		array[counter] = min;
+		counter++;
+		min++;
 	}
 	return (array);
 }
