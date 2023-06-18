2c484878c7d52aa3fff64b65fefdf39eee5489cf
moses946 <mosesmwaiw@gmail.com>
2023-06-18 17:09:36 +0300

    Fix atoi error

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,6 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+  * _atoi - Converts string to int
+  * @str: String to be converted
+  *
+  * Return: New int value or NULL if error
+  */
+int _atoi(char *str)
+{
+	int result = 0;
+
+	while (*str != '\0')
+	{
+		if (*str >= '0' && *str <= '9')
+		{
+			result = result * 10 + (*str - '0');
+			str++;
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+	return (result);
+}
+/**
   * main - Entry point
   * @argc: Number of arguments
   * @argv: Argument Vector
@@ -17,7 +41,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (1);
 	}
-	ans = atoi(argv[1]) * atoi(argv[2]);
+	ans = _atoi(argv[1]) * _atoi(argv[2]);
 	printf("%d\n", ans);
 	return (0);
 }
