922a517f694efcced28bc0c3c40e035eab70e6d5
moses946 <mosesmwaiw@gmail.com>
2023-06-22 23:43:12 +0300

    Change incluude stdarg file to .h

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,23 +1,66 @@
-#include <stdarg.c>
+#include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 /**
-  * print_numbers - Prints numbers followed by a new line
-  * @separator: String btw numbers
-  * @n: Number of arguments
+  * _puts - prints a string
+  * @str: string to be printed
+  */
+void _puts(const char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+}
+/**
+  * print_number - Prints an integer using _putchar
+  * @n: Integer to be printed
   *
+  */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
+/**
+  * print_numbers - prints numbers followed by a new line
+  * @separator: character to separate the numbers
+  * @n: number of integers passed to the function
   */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i = 0;
-	va_list ap;
+	unsigned int i;
+	va_list ptr;
+	
+	va_start(ptr, n);
 
-	va_start(ap, n);
-	while (i < n - 1)
+	if (n < 1)
 	{
-		printf("%d", va_arg(ap, int));
-		if (separator != '\0')
-			printf("%s", separator);
+		_putchar(' ');
+		return;
 	}
-	printf("%d\n", va_arg(ap, int));
-	va_end(ap);
+	for (i = 0; i < n - 1; i++)
+	{
+		if (separator != NULL)
+		{
+			print_number(va_arg(ptr, int));
+			_puts(separator);
+		}
+		else
+		{
+			print_number(va_arg(ptr, int));
+		}
+	}
+	print_number(va_arg(ptr, int));
+	_putchar('\n');
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,30 +1,75 @@
-#include <stdarg.h>
+#include <stdio.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
 /**
-  * print_strings - Print strings followed by a new line
-  * @separator: String to be printed between strings
-  * @n: Number of arguments
-  *
+  * _puts - prints a string
+  * @str: string to be printed
+  */
+void _puts(const char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+}
+/**
+  * print_strings - prints strings followed by a new line
+  * @separator: char to separate the strings
+  * @n: number of strings passed to the function
   */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+	const char *string;
 	va_list ptr;
-	unsigned int i = 0;
 
 	va_start(ptr, n);
-	while (i < n - 1)
+	
+	if (n < 1)
 	{
-		str = va_arg(ptr, char *);
-		if (str == NULL)
-			printf("(nil)");
-		else
-			printf("%s", str);
-		if (separator != NULL)
-			printf("%s", separator);
+		_putchar(' ');
+		return;
 	}
-	str = va_arg(ptr, char *);
-	if (str == NULL)
-		printf("(nil)\n");
+	for (i = 0; i < n - 1; i++)
+	{
+		string = va_arg(ptr, char*);
+		if (separator != NULL)
+		{
+			if (string == NULL)
+			{
+				_puts("(nil)");
+				_puts(separator);
+			}
+			else
+			{
+				_puts(string);
+				_puts(separator);
+			}
+		}
+		else
+		{
+			if (string == NULL)
+			{
+				_puts("(nil)");
+			}
+			else
+			{
+				_puts(string);
+			}
+		}
+	}
+	string = va_arg(ptr, char*);
+	if (string == NULL)
+	{
+		_puts("(nil)");
+		_putchar('\n');
+	}
 	else
-		printf("%s\n", str);
+	{
+		_puts(string);
+		_putchar('\n');
+	}
 }
+
