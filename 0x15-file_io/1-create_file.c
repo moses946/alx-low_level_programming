5b5446b8cdccb415f584c5e31b4473d7c061a564
moses946 <mosesmwaiw@gmail.com>
2023-07-13 13:10:16 +0300

    Add check for NULL text_content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,10 +34,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
-	if (*text_content)
+	if (text_content != NULL)
 	{
 		if ((write(fd, text_content, _strlen(text_content))) == -1)
 			return (-1);
