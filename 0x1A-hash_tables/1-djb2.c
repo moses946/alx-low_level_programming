d90ddbcaafbbd16ee11dafbd6d6fc520b2c6a731
moses946 <mosesmwaiw@gmail.com>
2023-09-14 19:21:06 +0300

    Initial commit

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+  * hash_djb2 - Implementation of djb2 algorithm
+  * @str: String
+  *
+  * Return: int value
+  */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c;
+	return (hash);
+}
